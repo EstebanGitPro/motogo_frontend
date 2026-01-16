7b0adef126b192896d4b5c12b36cc18fed58b04d
EstebanGitPro <estebanpoly.e.a@gmail.com>
2026-01-16 17:51:26 -0500

    FEAT: Add global service catalog entities and repository interfaces

--- lib/src/core/catalogs/domain/repositories/catalogs_repository.dart
+++ lib/src/core/catalogs/domain/repositories/catalogs_repository.dart
@@ -3,6 +3,7 @@ import 'package:motogo_frontend/src/core/catalogs/domain/entities/branch_type_en
 import 'package:motogo_frontend/src/core/catalogs/domain/entities/brand_entity.dart';
 import 'package:motogo_frontend/src/core/catalogs/domain/entities/city_entity.dart';
 import 'package:motogo_frontend/src/core/catalogs/domain/entities/department_entity.dart';
+import 'package:motogo_frontend/src/core/catalogs/domain/entities/service_entity.dart';
 import 'package:motogo_frontend/src/core/errors/error_model.dart';
 
 /// Repository interface for catalog operations.
@@ -20,4 +21,12 @@ abstract class CatalogsRepository {
 
   /// Fetches the list of branch establishment types.
   Future<Either<ErrorModel, List<BranchTypeEntity>>> getBranchTypes();
+
+  /// Fetches the list of services from the global catalog.
+  Future<Either<ErrorModel, List<ServiceEntity>>> getServices({
+    String? serviceType,
+  });
+
+  /// Fetches the list of service types.
+  Future<Either<ErrorModel, List<String>>> getServiceTypes();
 }
